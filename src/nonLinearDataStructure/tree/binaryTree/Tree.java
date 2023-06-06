0f38fe4c9345385e9a99ac7890656171de91fc0e
Victor Emanoel <victoremaanoel@outlook.com>
2023-06-05 23:04:06 -0300

    Implemented Tree class and method addElement()

--- src/nonLinearDataStructure/tree/binaryTree/Tree.java
+++ src/nonLinearDataStructure/tree/binaryTree/Tree.java
@@ -1,5 +1,39 @@
 package nonLinearDataStructure.tree.binaryTree;
 
-public class Tree {
-    
+import nonLinearDataStructure.tree.utils.Node;
+
+public class Tree<T extends Comparable<T>> {
+    Node<T> root;
+
+    public Tree(){
+        this.root = null;
+    }
+
+    public void addElement(T element){
+        Node<T> newElement = new Node<T>(element);
+        if(root == null){
+            this.root = newElement;
+            return;
+        }else{
+            Node<T> current = this.root;
+            while(true){
+                if(newElement.getElement().compareTo(current.getElement()) == -1){
+                    if(current.getLeft() != null){
+                        current = current.getLeft();
+                    }else{
+                        current.setLeft(newElement);
+                        break;
+                    }
+                }else {
+                    if(current.getRight() != null){
+                        current = current.getRight();
+                    }else {
+                        current.setRight(newElement);
+                        break;
+                    }
+                }
+            }
+        }
+        return;
+    }
 }
