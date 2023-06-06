9a3916fce55259b6efd950b1b5c388d879b47fc1
Victor Emanoel <victoremaanoel@outlook.com>
2023-06-05 22:41:03 -0300

    Implemented Node class - tree

--- src/nonLinearDataStructure/tree/utils/Node.java
+++ src/nonLinearDataStructure/tree/utils/Node.java
@@ -1,5 +1,39 @@
 package nonLinearDataStructure.tree.utils;
 
-public class Node {
+public class Node<T> {
+    private T element;
+    private Node<T> left;
+    private Node<T> right;
+
+    public Node(T value){
+        this.element = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+
     
 }
